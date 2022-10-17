0447a93f0135775371ae0def974259aa751c344b
Jesse Songok <jessesongok@gmail.com>
2022-10-17 11:11:09 +0300

    Create User Entity

---
+++ src/main/java/com/jessesongok/demo/firebase/FirebaseInitialization.java
@@ -0,0 +1,25 @@
+package com.jessesongok.demo.firebase;
+
+import com.google.auth.oauth2.GoogleCredentials;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.FirebaseOptions;
+import org.springframework.stereotype.Service;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@Service
+public class FirebaseInitialization {
+    public void initialization() throws IOException {
+
+        FileInputStream serviceAccount =  new FileInputStream("./stone-age-technologies-test-firebase-adminsdk-iq6rr-b98ba340b9.json");
+
+        FirebaseOptions options = new FirebaseOptions.Builder()
+                .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+                .build();
+
+        FirebaseApp.initializeApp(options);
+
+    }
+
+}
